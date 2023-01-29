c701677f5c01a411e377b4ea26d6722ef4f5ea1f
Mikhail Yatsenko <info@evo-dev.ru>
2023-01-29 10:35:47 +0500

    start

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OSPanel/domains/localhost/catalog/');
+define('DIR_SYSTEM', 'C:/OSPanel/domains/localhost/system/');
+define('DIR_IMAGE', 'C:/OSPanel/domains/localhost/image/');
+define('DIR_LANGUAGE', 'C:/OSPanel/domains/localhost/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OSPanel/domains/localhost/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OSPanel/domains/localhost/system/config/');
+define('DIR_CACHE', 'C:/OSPanel/domains/localhost/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OSPanel/domains/localhost/system/storage/download/');
+define('DIR_LOGS', 'C:/OSPanel/domains/localhost/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OSPanel/domains/localhost/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OSPanel/domains/localhost/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart_test');
+define('DB_PASSWORD', '22051991');
+define('DB_DATABASE', 'opencart_test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
